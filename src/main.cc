6ea025f7baddd2a5269ff5259690d7e6585e96b8
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-11-11 14:30:55 +0100

    added logger singleton class

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,11 @@
+#include "utils/log.h"
+
 #include <iostream>
+#include <memory>
 
 int main() {
-    std::cout << "Hello, World!\n";
+    auto log_instance = utils::Logger::get_instance();
+    log_instance->config(utils::Logger::LogLevel::Debug, std::cout);
 
     return 0;
 }
