af3ee4ce8adfd6a49776f318ad531cddf9bf1d82
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-11-21 20:04:49 +0100

    added `SFMLPlayerView` observer class

---
+++ src/observer/view/player.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef GAME_SRC_OBSERVER_VIEW_PLAYER_H
+#define GAME_SRC_OBSERVER_VIEW_PLAYER_H
+
+#include "SFML/Graphics.hpp"
+
+#include "../../subject/entity/entity.h"
+#include "../../utils/log.h"
+#include "../observer.h"
+
+namespace view {
+
+class PlayerViewSFML : public Observer<subject::Subject> {
+protected:
+    using Window = sf::RenderWindow;
+    using Player = subject::entity::Player;
+
+public:
+    inline PlayerViewSFML(Window* window) : window(window) {}
+    ~PlayerViewSFML() = default;
+
+    void notify(const subject::Subject& source, ObserverEvent event) override {
+        switch (event) {
+        case ObserverEvent::PositionChange:
+            LOG(Debug) << "Position change event called\n";
+            break;
+        default:
+            break;
+        }
+    }
+
+protected:
+    Window* window;
+};
+
+} // namespace view
+
+#endif // GAME_SRC_OBSERVER_VIEW_PLAYER_H
