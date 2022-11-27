612b44eb76d13accf84bc1041c616c9d92998c12
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-11-27 12:24:25 +0100

    added asset loading in `SFMLSubjectFactory`

--- src/factory.cc
+++ src/factory.cc
@@ -1,18 +1,65 @@
 #include "factory.h"
 
-#include "observer/view/player.h"
+#include "observer/view/subject.h"
+#include "utils/log.h"
+#include "utils/types.h"
 
-subject::Subject* SFMLSubjectFactory::create_subject(const Vec2& pos, const Bounds& bounds) const { return nullptr; }
+#include <stdexcept>
 
-subject::entity::Entity* SFMLSubjectFactory::create_entity(const Vec2& pos, const Bounds& bounds) const {
+SFMLSubjectFactory::SFMLSubjectFactory(std::shared_ptr<Window> window) : window(window) {
+    // instanciate assets
+    const sf::Vector2i player_texture_size{64, 64};
+    const sf::Vector2i tile_texture_size{64, 64};
+
+    const sf::Vector2i player_sprite_size{1, 1};
+    const sf::Vector2i tile_sprite_size{2, 2};
+
+    sf::FileInputStream spritesheet_stream;
+    if (!spritesheet_stream.open("assets/spritesheet.png")) {
+        LOG(Error) << "could not load spritesheet\n";
+        throw std::runtime_error("failed to load assets");
+    }
+
+    this->spritesheet = std::make_unique<sf::Texture>();
+    this->spritesheet->loadFromStream(spritesheet_stream);
+
+    auto player_sprite = std::make_shared<sf::Sprite>();
+    auto tile_sprite = std::make_shared<sf::Sprite>();
+
+    player_sprite->setTexture(*this->spritesheet);
+    tile_sprite->setTexture(*this->spritesheet);
+    player_sprite->setTextureRect(sf::IntRect({0, 0}, player_texture_size));
+    tile_sprite->setTextureRect(sf::IntRect({67, 0}, tile_texture_size));
+
+    player_sprite->setOrigin(0, player_texture_size.y);
+    tile_sprite->setOrigin(0, tile_texture_size.y);
+
+    player_sprite->setScale(static_cast<f64>(player_sprite_size.x) / static_cast<f64>(player_texture_size.x),
+                            static_cast<f64>(player_sprite_size.y) / static_cast<f64>(player_texture_size.y));
+    tile_sprite->setScale(static_cast<f64>(tile_sprite_size.x) / static_cast<f64>(tile_sprite_size.x),
+                          static_cast<f64>(tile_texture_size.y) / static_cast<f64>(tile_texture_size.y));
+
+    this->sprites = {{SubjectType::Player, player_sprite}, {SubjectType::Tile, tile_sprite}};
+}
+
+subject::Subject* SFMLSubjectFactory::create_subject(std::shared_ptr<Camera> cam, const Vec2& pos,
+                                                     const Bounds& bounds) const {
     return nullptr;
 }
 
-subject::entity::Player* SFMLSubjectFactory::create_player(const Vec2& pos, const Bounds& bounds) const {
+subject::entity::Entity* SFMLSubjectFactory::create_entity(std::shared_ptr<Camera> cam, const Vec2& pos,
+                                                           const Bounds& bounds) const {
+    return nullptr;
+}
+
+subject::entity::Player* SFMLSubjectFactory::create_player(std::shared_ptr<Camera> cam, const Vec2& pos,
+                                                           const Bounds& bounds) const {
     Player* player = new Player{pos, bounds};
-    auto observer = std::unique_ptr<Observer<Subject>>(new view::PlayerViewSFML(this->window));
+    auto observer = std::unique_ptr<Observer<Subject>>(
+        new view::SubjectViewSFML(this->window, this->sprites.at(SubjectType::Player)));
 
     player->subscribe(std::move(observer));
+    player->attach_camera(cam);
 
     return player;
 }
--- src/factory.h
+++ src/factory.h
@@ -7,25 +7,34 @@
 
 #include "subject/factory.h"
 
+#include <unordered_map>
+
 class SFMLSubjectFactory : public SubjectFactory {
 private:
     using Window = sf::RenderWindow;
+    using Sprite = sf::Sprite;
+
+private:
+    /// Enum of different drawable types (used for looking up sprites).
+    enum class SubjectType { Player, Tile };
 
 public:
-    inline SFMLSubjectFactory(std::shared_ptr<Window> window) : window(window) {}
+    SFMLSubjectFactory(std::shared_ptr<Window> window);
     ~SFMLSubjectFactory() = default;
 
     /// Construct subject with attached SFML specific view and return pointer.
-    Subject* create_subject(const Vec2& pos, const Bounds& bounds) const override;
+    Subject* create_subject(std::shared_ptr<Camera> cam, const Vec2& pos, const Bounds& bounds) const override;
 
     /// Construct entity with attached SFML specific view and return pointer.
-    Entity* create_entity(const Vec2& pos, const Bounds& bounds) const override;
+    Entity* create_entity(std::shared_ptr<Camera> cam, const Vec2& pos, const Bounds& bounds) const override;
 
     /// Construct player with attached SFML specific view and return pointer.
-    Player* create_player(const Vec2& pos, const Bounds& bounds) const override;
+    Player* create_player(std::shared_ptr<Camera> cam, const Vec2& pos, const Bounds& bounds) const override;
 
 private:
-    std::shared_ptr<Window> window;
+    std::shared_ptr<Window> window{};
+    std::unique_ptr<sf::Texture> spritesheet{};
+    std::unordered_map<SubjectType, std::shared_ptr<Sprite>> sprites{};
 };
 
 #endif // GAME_SRC_FACTORY_H
