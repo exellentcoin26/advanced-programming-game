9983ea2c1c9fb63b8fe57fc56b8bff94b244f781
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-11-28 11:16:54 +0100

    added `LevelLoader` class and tests to parse toml level files

---
+++ src/utils/levelloader.cc
@@ -0,0 +1,93 @@
+#include "levelloader.h"
+
+#include "tomlplusplus/toml.hpp"
+
+#include "types.h"
+
+LevelLoader::LevelInfo LevelLoader::load_level_from_file(std::string_view filepath) {
+    LevelLoaderExceptionVec exceptions{};
+
+    auto config = toml::parse_file(filepath);
+
+    // ===================
+    //    Load from file
+    // ===================
+
+    //      Level
+
+    const auto opt_level_map = config["level"]["map"].value<std::string_view>();
+
+    if (!opt_level_map.has_value()) {
+        exceptions.emplace_back("`level` value not set");
+    }
+
+    //      Camera
+
+    const u32 camera_height = config["camera"]["heigh"].value_or(2);
+    const f64 camera_increment = config["camera"]["increment"].value_or(0);
+
+    if (!exceptions.empty())
+        throw exceptions;
+
+    // ===================
+    //        Parse
+    // ===================
+
+    const std::string_view level_map{opt_level_map.value()};
+
+    std::stringstream level_map_stream{std::string(level_map)};
+
+    u32 map_width{0};
+    u32 map_height{0};
+
+    auto map_builder = LevelInfo::create(camera_height, camera_increment);
+
+    std::string line;
+    while (std::getline(level_map_stream, line, '\n')) {
+        if (map_width == 0 && line.size() != 0) {
+            // this is first line that is read, thus `map_width` is not yet set
+            map_width = line.size();
+        } else if (line.size() != map_width) {
+            // map is invalid
+            exceptions.emplace_back("map size is invalid");
+
+            throw exceptions;
+        }
+
+        for (unsigned int i = 0; i < line.size(); ++i) {
+            switch (line.at(i)) {
+            case 'x':
+                // tile
+                map_builder.add_tile(math::Vec2(i, map_height));
+                break;
+            case 'p':
+                // player
+                try {
+                    map_builder.set_player(math::Vec2(i, map_height));
+                } catch (const LevelLoaderExceptionVec& e) {
+                    exceptions.insert(exceptions.end(), e.begin(), e.end());
+                }
+                break;
+            case 'g':
+                try {
+                    map_builder.set_goal(math::Vec2(i, map_height));
+                } catch (const LevelLoaderExceptionVec& e) {
+                    exceptions.insert(exceptions.end(), e.begin(), e.end());
+                }
+                // goal
+                break;
+            default:
+                break;
+            }
+        }
+
+        ++map_height;
+    }
+
+    if (!exceptions.empty())
+        throw exceptions;
+
+    map_builder.set_size(math::Vec2(map_width, map_height));
+
+    return map_builder.build();
+}
---
+++ src/utils/levelloader.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#ifndef GAME_SRC_UTILS_LEVELLOADER_H
+#define GAME_SRC_UTILS_LEVELLOADER_H
+
+#include "../math/vec.h"
+#include "../world.h"
+
+#include <exception>
+#include <string_view>
+#include <vector>
+
+class LevelLoader {
+private:
+    class LevelLoaderException : public std::exception {
+    public:
+        inline LevelLoaderException(const std::string& message) : message(std::move(message)) {}
+
+        inline const char* what() const noexcept override { return message.c_str(); }
+
+    private:
+        std::string message{};
+    };
+
+    class LevelLoaderExceptionVec : public std::exception, public std::vector<LevelLoaderException> {
+    public:
+        LevelLoaderExceptionVec() = default;
+        ~LevelLoaderExceptionVec() = default;
+
+        inline const char* what() const noexcept override { return "failed to parse level, see content for details"; }
+    };
+
+public:
+    struct LevelInfo {
+    private:
+        class LevelInfoBuilder {
+        public:
+            inline LevelInfoBuilder(u32 camera_height, f64 camera_increment)
+                : camera_height(camera_height), camera_increment(camera_increment) {}
+
+            inline void set_size(const math::Vec2& size) {
+                if (this->size.has_value())
+                    throw LevelLoaderException("DEV: cannot set size twice");
+                this->size = size;
+            }
+
+            inline void add_tile(const math::Vec2& tile) { this->tiles.push_back(tile); }
+
+            inline void set_player(const math::Vec2& player) {
+                if (this->player.has_value())
+                    throw LevelLoaderException("cannot set player twice in level map");
+                this->player = player;
+            }
+
+            inline void set_goal(const math::Vec2& goal) {
+                if (this->goal.has_value())
+                    throw LevelLoaderException("cannot set goal twice in level map");
+                this->goal = goal;
+            }
+
+            inline LevelInfo build() const {
+                LevelLoaderExceptionVec exceptions;
+
+                // check that all atributes are set
+                if (!this->size.has_value())
+                    exceptions.emplace_back("DEV: size value is not set");
+
+                if (!this->player.has_value())
+                    exceptions.emplace_back("player is not set in level map");
+
+                if (!this->goal.has_value())
+                    exceptions.emplace_back("goal is not set in level map");
+
+                if (!exceptions.empty())
+                    throw exceptions;
+
+                return {this->size.value(), this->tiles,         this->player.value(),
+                        this->goal.value(), this->camera_height, this->camera_increment};
+            }
+
+        private:
+            std::optional<math::Vec2> size{};
+            std::vector<math::Vec2> tiles{};
+            std::optional<math::Vec2> player{};
+            std::optional<math::Vec2> goal{};
+            const u32 camera_height{};
+            const f64 camera_increment{};
+        };
+
+    public:
+        inline static LevelInfoBuilder create(u32 camera_height, f64 camera_increment) {
+            return {camera_height, camera_increment};
+        }
+
+    public:
+        const math::Vec2 size{};
+        const std::vector<math::Vec2> tiles{};
+        const math::Vec2 player{};
+        const math::Vec2 goal{};
+        const u32 camera_height{};
+        const f64 camera_increment{};
+    };
+
+public:
+    static LevelInfo load_level_from_file(std::string_view filepath);
+};
+
+#endif // GAME_SRC_UTILS_LEVELLOADER_H
