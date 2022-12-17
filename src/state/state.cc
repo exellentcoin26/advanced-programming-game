d00ff0816541a2d7228115e3f1882d1749a6f42c
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-12-17 17:31:54 +0100

    removed warnings

--- src/state/state.cc
+++ src/state/state.cc
@@ -6,8 +6,8 @@ State::State(std::shared_ptr<Window> window) : window(window) {}
 
 MenuState::MenuState(std::shared_ptr<Window> window) : State(window) {}
 
-void state::MenuState::update(Keyboard* keyboard) {}
+void state::MenuState::update(Keyboard* keyboard) { (void)keyboard; }
 
 OptionsState::OptionsState(std::shared_ptr<Window> window) : State(window) {}
 
-void state::OptionsState::update(Keyboard* keyboard) {}
+void state::OptionsState::update(Keyboard* keyboard) { (void)keyboard; }
